4b9e0516b1984b49a52e365a47e8181b62c76ec7
alamgirweb11 <alamgirweb11@gmail.com>
2022-03-06 10:57:32 +0600

    add transition and finish

--- src/App.vue
+++ src/App.vue
@@ -1,172 +1,174 @@
 <template>
-<div class="ctr">
-  <questions v-if="questionsAnswered < questions.length"
-   :questions="questions"
-   :questionsAnswered="questionsAnswered"
-   @question-answered="questionAnswered"
-   ></questions>
-  <answers v-else
-  :results="results"
-  :totalCorrect="totalCorrect"
-  ></answers>
-    <button type="button" class="reset-btn"
-    @click.prevent="resetQuestions()"
-    v-if="questionsAnswered === questions.length"
-    >Reset</button>
-</div>
+  <div class="ctr">
+    <transition name="fade" mode="out-in">
+      <questions
+        v-if="questionsAnswered < questions.length"
+        :questions="questions"
+        :questionsAnswered="questionsAnswered"
+        @question-answered="questionAnswered"
+      ></questions>
+      <answers v-else :results="results" :totalCorrect="totalCorrect"></answers>
+    </transition>
+    <button
+      type="button"
+      class="reset-btn"
+      @click.prevent="resetQuestions()"
+      v-if="questionsAnswered === questions.length"
+    >
+      Reset
+    </button>
+  </div>
 </template>
 
 <script>
-import Questions from '@/components/Questions';
-import Answers from '@/components/Answers';
+import Questions from "@/components/Questions";
+import Answers from "@/components/Answers";
 export default {
-  name: 'App',
-  data(){
-     return{
-       questionsAnswered: 0,
-       totalCorrect: 0,
-        questions: [
+  name: "App",
+  data() {
+    return {
+      questionsAnswered: 0,
+      totalCorrect: 0,
+      questions: [
         {
-            q: 'What is 2 + 2?', 
-            answers: [
-                {
-                    text: '4',
-                    is_correct: true
-                },
-                {
-                    text: '3',
-                    is_correct: false 
-                },
-                {
-                    text: '8',
-                    is_correct: false 
-                },
-                {
-                    text: '5',
-                    is_correct: false 
-                }
-            ] 
-        },
-        { 
-            q: 'How many letters are in the word "Banana"?', 
-            answers: [
-                {
-                    text: '5',
-                    is_correct: false
-                },
-                {
-                    text: '7',
-                    is_correct: false 
-                },
-                {
-                    text: '6',
-                    is_correct: true 
-                },
-                {
-                    text: '12',
-                    is_correct: false 
-                }
-            ] 
-        },
-        { 
-            q: 'Find the missing letter: C_ke', 
-            answers: [
-                {
-                    text: 'e',
-                    is_correct: false
-                },
-                {
-                    text: 'a',
-                    is_correct: true 
-                },
-                {
-                    text: 'i',
-                    is_correct: false 
-                },
-                {
-                    text: 'n',
-                    is_correct: false 
-                }
-            ] 
-        },
-        { 
-            q: 'Find the missing letter: Tig_r', 
-            answers: [
-                {
-                    text: 'f',
-                    is_correct: false
-                },
-                {
-                    text: 'a',
-                    is_correct: false 
-                },
-                {
-                    text: 'e',
-                    is_correct: true 
-                },
-                {
-                    text: 'n',
-                    is_correct: false 
-                }
-            ] 
+          q: "What is 2 + 2?",
+          answers: [
+            {
+              text: "4",
+              is_correct: true,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "8",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: false,
+            },
+          ],
         },
         {
-            q: 'What is 10 - 5?', 
-            answers: [
-                {
-                    text: '4',
-                    is_correct: false
-                },
-                {
-                    text: '3',
-                    is_correct: false 
-                },
-                {
-                    text: '8',
-                    is_correct: false 
-                },
-                {
-                    text: '5',
-                    is_correct: true 
-                }
-            ] 
-        },
-    ],
-    results: [
-        {
-            min: 0,
-            max: 3,
-            title: "Try again!",
-            desc: "Do a little more studying and you may succeed!"
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: "5",
+              is_correct: false,
+            },
+            {
+              text: "7",
+              is_correct: false,
+            },
+            {
+              text: "6",
+              is_correct: true,
+            },
+            {
+              text: "12",
+              is_correct: false,
+            },
+          ],
         },
         {
-            min: 5,
-            max: 5,
-            title: "Wow, you're a genius!",
-            desc: "Studying has definitely paid off for you!"
-        }
-    ]
-     }
+          q: "Find the missing letter: C_ke",
+          answers: [
+            {
+              text: "e",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: true,
+            },
+            {
+              text: "i",
+              is_correct: false,
+            },
+            {
+              text: "n",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "Find the missing letter: Tig_r",
+          answers: [
+            {
+              text: "f",
+              is_correct: false,
+            },
+            {
+              text: "a",
+              is_correct: false,
+            },
+            {
+              text: "e",
+              is_correct: true,
+            },
+            {
+              text: "n",
+              is_correct: false,
+            },
+          ],
+        },
+        {
+          q: "What is 10 - 5?",
+          answers: [
+            {
+              text: "4",
+              is_correct: false,
+            },
+            {
+              text: "3",
+              is_correct: false,
+            },
+            {
+              text: "8",
+              is_correct: false,
+            },
+            {
+              text: "5",
+              is_correct: true,
+            },
+          ],
+        },
+      ],
+      results: [
+        {
+          min: 0,
+          max: 3,
+          title: "Try again!",
+          desc: "Do a little more studying and you may succeed!",
+        },
+        {
+          min: 5,
+          max: 5,
+          title: "Wow, you're a genius!",
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
   },
   methods: {
-      questionAnswered(is_correct){
-           if(is_correct){
-               this.totalCorrect++
-           }
-           this.questionsAnswered++
-      },
-      resetQuestions(){
-              this.questionsAnswered = 0;
-              this.totalCorrect = 0;
+    questionAnswered(is_correct) {
+      if (is_correct) {
+        this.totalCorrect++;
       }
+      this.questionsAnswered++;
+    },
+    resetQuestions() {
+      this.questionsAnswered = 0;
+      this.totalCorrect = 0;
+    },
   },
   components: {
-      Questions,
-      Answers
-  }
-}
+    Questions,
+    Answers,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -1,29 +1,27 @@
 <template lang="">
-    <div class="result">
-        <div class="title"> {{ results[answerIndex].title }}</div>
-        <div class="desc">
-              {{ results[answerIndex].desc }}
-        </div>
+  <div class="result">
+    <div class="title">{{ results[answerIndex].title }}</div>
+    <div class="desc">
+      {{ results[answerIndex].desc }}
     </div>
+  </div>
 </template>
 <script>
 export default {
-     name:  'AnswersComponent',
-     props: ['results', 'totalCorrect'],
-     computed:{
-           answerIndex(){
-                 let index = 0;
-                this.results.forEach((e, i) => {
-                      if(e.min <= this.totalCorrect && e.max >= this.totalCorrect){
-                            index = i;
-                      }
-                });
+  name: "AnswersComponent",
+  props: ["results", "totalCorrect"],
+  computed: {
+    answerIndex() {
+      let index = 0;
+      this.results.forEach((e, i) => {
+        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+          index = i;
+        }
+      });
 
-                return index;
-           }
-     }
-}
+      return index;
+    },
+  },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
